b614ab947a6d473df8ee955ac662e36f2d316876
Gregory Bell <trihard.studios@outlook.com>
2024-12-02 11:00:18 -0700

    start work on main page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h2>Gregory Bell</h2>
+</template>
---
+++ src/views/PortfolioView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h2>Projects</h2>
+</template>
