28ba1af89d8d5faac6e02f7644d1ad0841e67e5d
Gregory Bell <trihard.studios@outlook.com>
2024-12-07 00:40:47 -0700

    write a bit on the home page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterLink, RouterView} from 'vue-router'
 </script>
 
 <template>
-  <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/portfolio">Portfolio</RouterLink>
-      </nav>
-  </header>
-  <RouterView />
+    <header>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/portfolio">Portfolio</RouterLink>
+        </nav>
+    </header>
+    <RouterView/>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,5 +2,28 @@
 </script>
 
 <template>
-  <h2>Gregory Bell</h2>
+    <h2>Gregory Bell</h2>
+
+    <h3>About</h3>
+    <p>
+        Hello! My name is Gregory Bell and I am a student at the Colorado School of Mines in Computer Science.
+        I am expecting to graduate with my B.S. in May 2025 and my M.S. in December 2025.
+    </p>
+    <p>
+        I am a software engineer proficient in a wide variety of technologies with a proven track record for reliability.
+        I currently do a lot of full stack web development and develop tools for educators to simplify the grading process
+        for large classes.
+    </p>
+    <p>
+        When I'm not working, I enjoy running, hiking, and rock climbing, basically anything that gets me outdoors and gets
+        my heart rate up.
+        I am a passionate teacher having TA'd introductory computer science courses at Mines for 4 years and having helped
+        develop course content delivered to over two thousand students.
+    </p>
+    <p>
+        I am a firm believer in the power of progress; I always want to be able to took at myself a year down the line
+        and be proud of where I am.
+    </p>
+
+
 </template>
